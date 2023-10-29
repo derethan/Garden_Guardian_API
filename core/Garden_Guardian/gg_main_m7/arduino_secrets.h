f455aa58a9dae9376a5d39f84818f235bc9cf5d4
Andrew Patterson <apatterson93@gmail.com>
2023-10-29 03:23:40 -0230

    Removed Core, put in seperate repo

--- core/Garden_Guardian/gg_main_m7/arduino_secrets.h
+++
@@ -1,5 +0,0 @@
-//arduino_secrets.h header file
-
-#define SECRET_SSID "BATECH_Router"
-
-#define SECRET_PASS "Norton66"
--- core/Garden_Guardian/gg_main_m7/relay_control.h
+++
@@ -1,8 +0,0 @@
-void setRelay1 (int RELAY_PIN, float temperature1, float targetTemperature) {
-      // Relay 1
-    if (temperature1 <= targetTemperature) {
-        digitalWrite(RELAY_PIN, LOW);
-    } else {
-        digitalWrite(RELAY_PIN, HIGH);
-    }
-}
