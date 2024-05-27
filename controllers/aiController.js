b87e4ee11c96d8c6d045b71be08b812fcff743ab
Andrew Patterson <apatterson93@gmail.com>
2024-05-27 16:59:07 -0230

    Update aiController.js
    Added cache for AI results (Need local fs storage for long term cache)
    
    Added OpenAI Library
    
    Added generatePlantInfo  Function to generate Plant information based off of the parameters requested. Data is generated in JSON format from LLM

--- controllers/aiController.js
+++ controllers/aiController.js
@@ -2,7 +2,18 @@
  *  This Controller is used to handle the
  *  API requests to the OpenAI API
  * ************************************/
+
+//Import the OpenAI Library
+const OpenAI = require("openai");
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+// Import the Axios library
 const axios = require("axios");
+
+// Cache to store the responses from the AI
+const cache = {};
 
 const getPlantDescription = async (req, res) => {
   const url = process.env.AI_URL;
@@ -47,4 +58,81 @@ const getPlantDescription = async (req, res) => {
   }
 }; // end of getPlantDescription
 
-module.exports = { getPlantDescription };
+//Function to Generate Plant infromation for user Added Plants
+const generatePlantInfo = async (req, res) => {
+  //Format the request to lowercase for consistency
+  const plant = req.body.plantName ? req.body.plantName.toLowerCase() : null;
+  const variety = req.body.variety ? req.body.variety.toLowerCase() : null;
+
+  const properties = req.body.properties;
+
+  // Check if the request is already in the cache
+  const cacheKey = `${plant}-${variety}`;
+
+  if (cache[cacheKey]) {
+    console.log("Cache hit");
+    res.status(200).json(cache[cacheKey]);
+    return;
+  }
+
+  if (!plant) {
+    res.status(201).json({ error: "Plant name is required" });
+    return;
+  }
+
+  try {
+    // Make a request to the OpenAI API
+    const completion = await openai.chat.completions.create({
+      messages: [
+        {
+          role: "system",
+          content:
+            "You are a Expert Gardner,  Farmer and Instructor designed to output JSON",
+        },
+        {
+          role: "user",
+          content: `What can you tell me about the following properties for the plant ${plant}${
+            variety ? ` , variety ${variety}` : ""
+          }?
+          
+          ${properties.map((property) => {
+            return property.title + " ";
+          })}
+  
+          maintain the property names and values in the same order as the input.
+
+          If you don't have information about the plant or variety, please respond with "no information available"
+          `,
+        },
+      ],
+      model: "gpt-4o",
+      response_format: { type: "json_object" },
+    });
+
+    // Extract the response from the completion
+    const responseInfo = JSON.parse(completion.choices[0].message.content);
+    console.log(responseInfo);
+
+    // Check if the response was "no information available"  - AI could not find any information
+    if (responseInfo.Description === "no information available") {
+      console.log("No information available");
+
+      cache[cacheKey] = "No information available";
+      res.status(201).json("No information available");
+      return;
+    }
+
+    // Cache the response
+    cache[cacheKey] = responseInfo;
+
+    // Send the response to the client
+    res.status(200).json(responseInfo);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      error: "Server Connection Error: Failed to connect to the OpenAI API",
+    });
+  }
+}; // end of generatePlantInfo
+
+module.exports = { getPlantDescription, generatePlantInfo };
