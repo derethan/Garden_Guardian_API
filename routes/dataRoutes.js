85fb4d26475e7997cfd99f88ebbe98c97cf53b60
Andrew Patterson <apatterson93@gmail.com>
2024-06-10 16:36:05 -0230

    Refactor/ New Route
    Refactored the Naming to better align with RESTful API's.
    
    Added route "/plants/:plant/varieties"

--- routes/dataRoutes.js
+++ routes/dataRoutes.js
@@ -7,11 +7,24 @@ router.get('/status', (req, res) => {
   res.status(200).json({ status: 'online' });
 });
 
-//Define data Route to get plant from the datatbase
-router.get('/plants/all', dataController.getAllPlants);
+//Define data Route to Add a new plant to the database
+router.post('/plants', dataController.addPlant);
 
-// Define routes to Add, Update, etc plants in the database
-router.post('/plants/add', dataController.addPlant);
+//Define data Route to Get All plants from the datatbase
+router.get('/plants', dataController.getAllPlants);
+
+//Define data Route to Get Details of a Specified plant
+// router.get('/plants/:plant', dataController.getPlantDetails);
+
+// Define data Route to Get All Varieties for an a selected plant
+router.get('/plants/:plant/varieties', dataController.getPlantVariety);
+
+//Define data Route to Get Details of a specified plant variety
+// router.get('/plants/:plant/varieties/:variety', dataController.getVarietyDetails);
+
+
+
+
 
 
 
