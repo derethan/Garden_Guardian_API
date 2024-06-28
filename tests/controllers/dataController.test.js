3a4b9c29cf85b272b44d2f1ca3c5b7db81b361db
Andrew Patterson <apatterson93@gmail.com>
2024-06-28 03:03:55 -0230

    Create dataController.test.js

---
+++ tests/controllers/dataController.test.js
@@ -0,0 +1,13 @@
+// const jest = require('jest');
+
+describe('dataController', () => {
+
+    test ('adds 1 + 2 to equal 3', () => {
+        expect(1 + 2).toBe(3);
+    });
+
+    test ('adds 2 + 2 to equal 4', () => {
+        expect(2 + 2).toBe(4);
+    });
+
+});
