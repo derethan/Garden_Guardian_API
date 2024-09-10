4d1addaa046b39ab9040ab5d2fdb018cced2caef
Andrew Patterson <apatterson93@gmail.com>
2024-09-09 21:59:00 -0230

    Progress update

--- tests/controllers/dataController.test.js
+++ tests/controllers/dataController.test.js
@@ -1,13 +1,99 @@
-// const jest = require('jest');
+const { addPlant } = require("../../controllers/dataController");
+const dbQueryPromise = require("../db/dbConnect");
 
-describe('dataController', () => {
+// Use Jest to mock the dbQueryPromise function
+jest.mock("../db/dbConnect");
 
-    test ('adds 1 + 2 to equal 3', () => {
-        expect(1 + 2).toBe(3);
+describe("dataController", () => {
+  test("addPlant function -  It should return a 201 status code", async () => {
+
+    // Mock the request and response objects
+    const req = {
+      body: {
+        plant: "tomato",
+        variety: "roma",
+        properties: [
+          {
+            title: "Description",
+            value: "",
+          },
+          {
+            title: "Harvest Time",
+            value: "",
+          },
+          {
+            title: "How to Sow",
+            value: "",
+          },
+          {
+            title: "Spacing",
+            value: "",
+          },
+          {
+            title: "Grows Well With",
+            value: "",
+          },
+          {
+            title: "Avoid Planting With",
+            value: "",
+          },
+        ],
+      },
+    };
+    const res = {
+      status: jest.fn(() => res),
+      json: jest.fn(),
+    };
+
+    dbQueryPromise.mockResolvedValueOnce({
+      rows: [
+        {
+          plant: "tomato",
+          variety: "roma",
+            properties: [
+                {
+                title: "Description",
+                value: "",
+                },
+                {
+                title: "Harvest Time",
+                value: "",
+                },
+                {
+                title: "How to Sow",
+                value: "",
+                },
+                {
+                title: "Spacing",
+                value: "",
+                },
+                {
+                title: "Grows Well With",
+                value: "",
+                },
+                {
+                title: "Avoid Planting With",
+                value: "",
+                },
+            ],
+        },
+      ],
     });
 
-    test ('adds 2 + 2 to equal 4', () => {
-        expect(2 + 2).toBe(4);
-    });
+    await addPlant(req, res);
 
+    expect(res.status).toHaveBeenCalledWith(201);
+  });
 });
+
+// describe('dataController', () => {
+
+//     test ('adds 1 + 2 to equal 3', () => {
+//         expect(1 + 2).toBe(3);
+//     });
+
+//     test ('adds 2 + 2 to equal 4', () => {
+//         expect(2 + 2).toBe(4);
+//     });
+
+// });
